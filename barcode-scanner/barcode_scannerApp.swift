98bb7a1c59db1d83c1edf0383a81707f58381d8b
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-04-22 12:54:25 -0300

    Initial Commit

---
+++ barcode-scanner/barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_scannerApp.swift
+//  barcode-scanner
+//
+//  Created by Andres Rechimon on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
